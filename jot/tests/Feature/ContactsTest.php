734340493b14ddcb25719d78f07bdf6c680357c2
Gavin Kimpson <gkimpson@gmail.com>
2019-09-09 22:40:39 +0100

    added first working test using TDD

--- jot/tests/Feature/ContactsTest.php
+++ jot/tests/Feature/ContactsTest.php
@@ -2,21 +2,22 @@
 
 namespace Tests\Feature;
 
+use App\Contact;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ContactsTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_contact_can_be_added()
+    {
+        $this->withoutExceptionHandling();
+        $this->post('/api/contacts', ['name' => 'Test Name']);
+
+        $this->assertCount(1, Contact::all());
     }
 }
